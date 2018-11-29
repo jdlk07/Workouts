4d5a28debc4ea4cc08605a9fa8cfceebd7e02878
Janath Dinesh Perera <jdlk07@gmail.com>
2018-11-29 16:42:11 +0530

    cleaned up directory files

--- static/main.js
+++
@@ -1,5 +0,0 @@
-var loadExercise = document.getElementById("loadExercise");
-loadExercise.addEventListener("click", function() {
-  var request = new XMLHttpRequest();
-  request.open('GET', currentURL + )
-})
